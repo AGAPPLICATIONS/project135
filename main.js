a9aebbb5a1a037371181a3a924d6a0cfc0b9c909
AGAPPLICATIONS <90456136+AGAPPLICATIONS@users.noreply.github.com>
2023-10-22 11:04:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+status1="";
+objects=[];
 function preload(){
 
 }
@@ -9,6 +11,29 @@ video.hide();
 }
 function draw(){
 image(video,0,0,470,360);
+if(status1 == true){
+    model.detect(video,gotResults);
+    for (let i = 0; i < objects.length; i++) {
+        document.getElementById("modelstatus").innerHTML="Status:Objects Detected";
+        fill("yellow");
+        confidence=floor(objects[i].confidence*100);
+        text(objects[i].label+" "+ confidence+"%",objects[i].x+15,objects[i].y+15);
+        noFill();
+        stroke("red");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        if (objects[i].label == input) {
+            document.getElementById("objectstatus").innerHTML="Object found";
+            synth=window.speechSynthesis;
+           var utterThis= new SpeechSynthesisUtterance(input+" found in image");
+           synth.speak(utterThis);
+        }else{
+            document.getElementById("objectstatus").innerHTML="Object not found";
+            synth=window.speechSynthesis;
+           var utterThis= new SpeechSynthesisUtterance(input+" not found in image");
+           synth.speak(utterThis);
+        }
+    }
+}
 }
 function start(){
 model= ml5.objectDetector('cocoSSD',modelloaded);
@@ -19,5 +44,16 @@ function modelloaded(){
     console.log("Model is loaded");
     status1=true;
     console.log(input);
+
+
+}
+function gotResults(error,results){
+if(error){
+    console.log(error);
+
+}else{
+    console.log(results);
+    objects=results;
+}
 
 }
